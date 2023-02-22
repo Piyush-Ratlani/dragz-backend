10dcb692c8a6d77ca82d65f535f5cc87a6e5a6d1
Piyush-Ratlani <69811087+Piyush-Ratlani@users.noreply.github.com>
2023-02-22 10:21:41 +0530

    updated edit service-category

--- src/controllers/serviceCategory.controller.js
+++ src/controllers/serviceCategory.controller.js
@@ -70,78 +70,65 @@ module.exports.addServiceCategory_post = async (req, res) => {
 module.exports.editServiceCategory_post = async (req, res) => {
   const { name, position } = req.body;
   const { categoryId } = req.params;
-  const category = await ServiceCategory.findById(categoryId);
-  const oldPosition = category.position;
-  const newPosition = position;
-  const serviceCategoryCount = await ServiceCategory.countDocuments();
+  try {
+    const category = await ServiceCategory.findById(categoryId);
+    const serviceCategoryCount = await ServiceCategory.countDocuments();
 
-  if (!name || !position) return errorRes(res, 400, "Invalid request.");
-  if (!category) return errorRes(res, 404, "Service category does not exist");
+    if (!name || !position) return errorRes(res, 400, "Invalid request.");
+    if (!category) return errorRes(res, 404, "Service category does not exist");
 
-  if (position <= 0)
-    return errorRes(
-      res,
-      400,
-      "Service-Category's position should be greater than 0."
-    );
-  else if (position > serviceCategoryCount)
-    return errorRes(
-      res,
-      400,
-      `Service-Category's position cannot be greater than ${serviceCategoryCount}.`
-    );
+    if (position <= 0)
+      return errorRes(
+        res,
+        400,
+        "Service-Category's position should be greater than 0."
+      );
+    else if (position > serviceCategoryCount)
+      return errorRes(
+        res,
+        400,
+        `Service-Category's position cannot be greater than ${serviceCategoryCount}.`
+      );
 
-  // if (newPosition = oldPosition){
-  //   category.name = name
-  //   await category.save()
-  //   .then(updatedCategory => {
-  //    const {_id, name, displayImage, position} = updatedCategory
-  //    return successRes(res, {updatedCategory: {_id, name, displayImage, position}, message: "Service category updated successfully."})
-  //   })
-  // }else
-  if (newPosition > oldPosition) {
-    const categoriesToUpdate = await ServiceCategory.find({
-      position: { $gt: oldPosition, $lte: newPosition },
-    });
-    await Promise.all(
-      categoriesToUpdate.map(async category => {
-        category.position--;
-        await category.save();
+    const oldPosition = category.position;
+    const newPosition = position;
+    if (newPosition > oldPosition) {
+      const categoriesToUpdate = await ServiceCategory.find({
+        position: { $gt: oldPosition, $lte: newPosition },
+      });
+      await Promise.all(
+        categoriesToUpdate.map(async category => {
+          category.position--;
+          await category.save();
+        })
+      );
+    } else if (newPosition < oldPosition) {
+      const categoriesToUpdate = await ServiceCategory.find({
+        position: { $gte: newPosition, $lt: oldPosition },
+      });
+      await Promise.all(
+        categoriesToUpdate.map(async category => {
+          category.position++;
+          await category.save();
+        })
+      );
+    }
+
+    category.name = name;
+    category.position = position;
+    await category
+      .save()
+      .then(updatedCategory => {
+        const { _id, name, displayImage, position } = updatedCategory;
+        return successRes(res, {
+          updatedCategory: { _id, name, displayImage, position },
+          message: "Service category updated successfully.",
+        });
       })
-    );
-  } else if (newPosition < oldPosition) {
-    const categoriesToUpdate = await ServiceCategory.find({
-      position: { $gte: newPosition, $lt: oldPosition },
-    });
-    await Promise.all(
-      categoriesToUpdate.map(async category => {
-        category.position++;
-        await category.save();
-      })
-    );
+      .catch(err => internalServerError(res, err));
+  } catch (err) {
+    internalServerError(res, err);
   }
-
-  category.name = name;
-  category.position = position;
-  await category.save().then(updatedCategory => {
-    const { _id, name, displayImage, position } = updatedCategory;
-    return successRes(res, {
-      updatedCategory: { _id, name, displayImage, position },
-      message: "Service category updated successfully.",
-    });
-  });
-
-  // ServiceCategory.findByIdAndUpdate(categoryId, updates, { new: true })
-  //   .then(updatedCategory => {
-  //     if (!updatedCategory)
-  //       return errorRes(res, 400, "Category does not exist.");
-  //     else
-  //       return successRes(res, {
-  //         updatedCategory,
-  //         message: "Category updated successfully.",
-  //       });
-  //   })
-  //   .catch(err => internalServerError(res, err));
 };
 
 module.exports.deleteServiceCategory_delete = async (req, res) => {
